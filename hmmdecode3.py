8955de1e028d2e181c68a8e43d8c7bd158341b81
kabirchopra96 <49553123+kabirchopra96@users.noreply.github.com>
2019-06-13 03:10:01 -0700

    Update hmmdecode3.py

--- hmmdecode3.py
+++ hmmdecode3.py
@@ -14,16 +14,19 @@ prob_of_tags = defaultdict(int)
 test=open(sys.argv[1], "r", encoding="utf8")
 for sentence in test:
     prev_tags = {}
+    # Split the sentence into words
     words = sentence.strip().split(' ')
     #if new word
     if(words[0] not in e_p.keys()):
         for each_tag in tags:
             prob_of_tags[each_tag] = t_p[each_tag]['START']
             prev_tags[(each_tag, 0)] = 'START'
+    # if seen word
     else:
         for each_tag in e_p[words[0]].keys():
             prob_of_tags[each_tag] = e_p[words[0]][each_tag] + t_p[each_tag]['START']
             prev_tags[(each_tag, 0)] = 'START'
+    # For each word
     for k in range(1, len(words)):
         a = {}
         # if new word
@@ -32,6 +35,7 @@ for sentence in test:
                 a[each_tag] = 0.0
             e_p[words[k]] = a
         t = {}
+        # for each tag, calculate probability for the current word
         for tag1 in e_p[words[k]].keys():
             max1 = float('-Inf')
             for tag0 in prob_of_tags.keys():
@@ -42,6 +46,7 @@ for sentence in test:
             t[tag1] = max1 + e_p[words[k]][tag1]
             prev_tags[(tag1, k)] = best
         prob_of_tags = t
+    # best : Most probable tags
     best = max(prob_of_tags, key=prob_of_tags.get)
     sentence_with_tags = words[len(words)-1] + "/" + best + " "
     for i in range(len(words)-1, 0, -1):
